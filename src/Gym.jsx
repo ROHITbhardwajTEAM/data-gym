84e01781b10f968108c42c505ff83b8607204a52
ROHITbhardwajTEAM <rohitwaj999@gmail.com>
2023-05-26 13:02:14 +0530

    v1

---
+++ src/Gym.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./gym.css";
+const Gym = () => {
+  return (
+    <div>
+      <div className="first"><img src="./cable.gif" alt="Girl in a jacket" width="165px" height="165px"/></div>
+      <div className="first set"><img src="./cable.gif" alt="Girl in a jacket" width="165px" height="165px"/></div>
+      <br />
+      <div className="first"></div>
+      <div className="first set"></div>
+
+      <br />
+      <div className="first"></div>
+      <div className="first set"></div>
+      <br />
+      <div className="first"></div>
+      <div className="first set"></div>
+    </div>
+  );
+};
+
+export default Gym;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+import Gym from "./Gym";
+import "./gym.css";
+
+ReactDOM.render(
+  <>
+<h1>Weekend workout</h1>
+      <h2>monday</h2>
+      <h3>Chest & biceps</h3>
+  <Gym/>
+  <h2>tuesday</h2>
+      <h3>Chest</h3>
+  <Gym/>
+  <h2>wednesday</h2>
+      <h3>Chest</h3>
+  <Gym/>
+  <h2>thursday</h2>
+      <h3>Chest</h3>
+  <Gym/>
+  <h2>friday</h2>
+      <h3>Chest</h3>
+  <Gym/>
+  <h2>saturday</h2>
+      <h3>Chest</h3>
+  <Gym/>
+
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
